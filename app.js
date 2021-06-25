9bcac0883e040347dc82242cb65437810ccfdc86
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-06-25 15:01:29 +0300

    input-file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const btnDownload = document.querySelectorAll('[data-upload]');
+const btnChange = document.querySelectorAll('[data-changePic]');
+const inputFileOne = document.querySelectorAll('.uploadFile-inputOne');
+const inputFileTwo = document.querySelectorAll('.uploadFile-inputTwo');
 
 btnDownload.forEach(item => {
-    item.addEventListener('click', event => {
-        event.preventDefault();
+    item.addEventListener('change', event => {
         let $this = event.currentTarget;
         let uploadId = $this.getAttribute('data-upload')
         let load = document.getElementById(uploadId)
@@ -14,14 +16,52 @@ btnDownload.forEach(item => {
     })
 })
 
-const btnChange = document.querySelectorAll('[data-changePic]');
 
 btnDownload.forEach(item => {
-    item.addEventListener('click', event => {
-        event.preventDefault();
+    item.addEventListener('change', event => {
         let $this = event.currentTarget;
         let uploadId = $this.getAttribute('data-changePic')
         let load = document.getElementById(uploadId)
         load.classList.add('done')
     })
 })
+
+inputFileOne.forEach(function(el) {
+
+    let textSelector = document.querySelector('.uploadFile-textOne')
+    let fileList;
+
+    el.addEventListener('change', e => {
+            fileList = []
+            for (let i = 0; i < el.files.length; i++) {
+                fileList.push(el.files[i]);
+            } 
+
+            fileList.forEach( file => {
+                textSelector.textContent = file.name
+            })
+    })
+})
+
+inputFileTwo.forEach(function(el) {
+
+    let textSelector = document.querySelector('.uploadFile-textTwo')
+    let fileList;
+
+    el.addEventListener('change', e => {
+            fileList = []
+            for (let i = 0; i < el.files.length; i++) {
+                fileList.push(el.files[i]);
+            } 
+
+            fileList.forEach( file => {
+                textSelector.textContent = file.name
+            })
+    })
+})
+
+
+
+
+
+
